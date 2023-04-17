d895c0207ccfd7f5223dda2d229e4d97a3372ba3
dmitr <dmitrytsarik@gmail.com>
2023-04-17 18:16:18 +0300

    git init

---
+++ src/main/java/ua/hillel/tsaryk/lessons/lesson2/Main.java
@@ -0,0 +1,12 @@
+package ua.hillel.tsaryk.lessons.lesson2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double flagLatitude = 48.455382926430744;
+
+        double flagLongitude = 35.06337457684211;
+
+        System.out.println("DniproFlag Latitude:" + flagLatitude + " Longitude:" + flagLongitude);
+    }
+}
---
+++ src/main/java/ua/hillel/tsaryk/lessons/lesson3/Main.java
@@ -0,0 +1,4 @@
+package ua.hillel.tsaryk.lessons.lesson3;
+
+public class Main {
+}
