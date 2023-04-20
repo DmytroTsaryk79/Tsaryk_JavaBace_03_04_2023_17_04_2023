99868afe9ea7af63c3aa8e3615a508ffa5a34e78
dmitr <dmitrytsarik@gmail.com>
2023-04-20 18:02:32 +0300

    test push

---
+++ src/main/java/ua/hillel/tsaryk/lessons/lesson5/Main.java
@@ -0,0 +1,19 @@
+package ua.hillel.tsaryk.lessons.lesson5;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 11;
+        int b = 13;
+        int c = 27;
+
+        int number = 4;
+
+        int volume = a * b * c;
+
+        int length = a * number + b * number + c * number;
+
+        System.out.println(" Volume = " + volume);
+        System.out.println(" Length = " + length);
+    }
+}
